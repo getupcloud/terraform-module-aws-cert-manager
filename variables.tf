63cffd960cffcaed46b5ae09b61e821ad7851ca8
Lanay M <marqueslanay@gmail.com>
2021-10-25 00:11:43 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,13 +18,13 @@ variable "cluster_oidc_issuer_url" {
 }
 
 variable "service_account_namespace" {
-  description = "Namespace of ServiceAccount for cluster-autoscaler controller"
-  default     = "velero"
+  description = "Namespace of ServiceAccount for cert-manager"
+  default     = "cert-manager"
 }
 
 variable "service_account_name" {
-  description = "ServiceAccount name for cluster-autoscaler controller"
-  default     = "eks-infra-velero"
+  description = "ServiceAccount name for cert-manager"
+  default     = "eks-infra-certmanager"
 }
 variable "tags" {
   description = "AWS tags to apply to resources"
